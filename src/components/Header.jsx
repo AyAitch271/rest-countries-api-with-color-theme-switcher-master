de7b742516f16e8b369323b12216c28e4d9455ed
AyAitch271 <154345991+AyAitch271@users.noreply.github.com>
2025-06-06 13:10:26 +0200

    .

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ import { DarkLightIcon } from "./icons/DarkLightIcon"
 
 export const Header = () => {
     return (
-        <header className="flex  justify-between items-center bg-element h-14 px-10">
+        <header className="flex justify-between items-center bg-element h-14 px-10">
             <h2 className="font-extrabold"><Link to='/'>Where in the world ?</Link></h2>
             <div className="flex items-center gap-2">
             <span className="size-6 fill-text">
